7170c807da9911ad70a9de7f08c346b44d4d16ef
AndouTsudumi <55andouTsudumi25@gmail.com>
2023-02-07 12:33:32 +0900

    途中経過、音が出ないが原因は不明
    Progress on the way, no sound, but cause unknown.

---
+++ MyDxLibGame3D/main.cpp
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------------
+// @brief  メイン処理.
+// 2016 Takeru Yui All Rights Reserved.
+//-----------------------------------------------------------------------------
+// インクルード
+#include "pch.h"
+
+// 関数.
+void InitialSettingWindow();
+
+//-----------------------------------------------------------------------------
+// @brief  メイン関数.
+//-----------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() < 0)
+	{
+		return -1;	// エラーが起きたら直ちに終了
+	}
+
+	// windowの初期設定.
+	InitialSettingWindow();
+
+	Sound* sound = new Sound(filePassAry[FileName::sadMemory]);
+
+	// ループ文.
+	while (!CheckHitKey(KEY_INPUT_ESCAPE))
+	{
+		ClearDrawScreen();
+
+		sound->Updata();
+
+		ScreenFlip();
+	}
+
+	sound->~Sound();
+
+	// ＤＸライブラリの後始末
+	DxLib_End();
+
+	// ソフトの終了
+	return 0;
+}
+
+/// <summary>
+/// Windowの初期設定関数.
+/// </summary>
+void InitialSettingWindow()
+{
+	//画面モードのセット
+	//SetGraphMode(640, 480, 16);
+	/*SetGraphMode( SCREEN_W, SCREEN_H, 32);*/
+
+	ChangeWindowMode(TRUE);
+
+}
---
+++ MyDxLibGame3D/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+// 以下インクルードファイル.
+#include "DxLib.h"
+
+#include "Sound.h"
+
+
+// 以下固定変数.
+
+// スクリーン画面変数.
+const int screenW = 640;     // 画面の解像度(横幅).
+const int screenH = 480;     // 画面の解像度(縦幅).
+const int screenG = 16;      // 画面のカラービット数.
