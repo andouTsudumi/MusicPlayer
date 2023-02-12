985213465a87dd8ac72fbd839c2c96376a66c9c7
AndouTsudumi <55andouTsudumi25@gmail.com>
2023-02-12 16:00:25 +0900

    main.cppの変更
    changed to main.cpp

--- MyDxLibGame3D/main.cpp
+++ MyDxLibGame3D/main.cpp
@@ -16,7 +16,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// ＤＸライブラリ初期化処理
 	if (DxLib_Init() < 0)
 	{
-		return -1;	// エラーが起きたら直ちに終了
+		return -1;	// エラーが起きたら直ちに終了.
 	}
 
 	// windowの初期設定.
@@ -55,4 +55,39 @@ void InitialSettingWindow()
 
 	ChangeWindowMode(TRUE);
 
+	// ウィンドウの見た目を変更する.
+	SetWindowStyleMode(7);
+	/*
+	* [ウィンドウ]の見た目を変更する
+	*`Mode`
+	* 0` デフォルト
+	* 1` タイトルバーなし
+	* 2` タイトルバーなし枠なし
+	* 3` 枠なし
+	* 4` 何もなし
+	* 5` 最小化無し
+	* 6` ツールバーウインドウ
+	* 7` 最大化つきウインドウ、初期状態が通常サイズ
+	* 8` 最大化つきウインドウ、初期状態が最大化状態
+	* 9` デフォルトに縁の立体化無し
+	* 10` 最大化つきウインドウ、縁の立体化無し
+	* 11` 閉じる無し、最小化無し
+	*  戻り値
+	* 0` 成功
+	* -1` 失敗
+	*/
+
+	// 画面のサイズを変更できるようにするかどうか.
+	SetWindowSizeChangeEnableFlag(TRUE,FALSE);
+	/*
+	* flag
+	* true：変更できるようにする.
+	* false：変更できないようにする.
+	* 
+	* 戻り値
+	* 0：成功
+	* -1：エラー
+	*/
+
+	SetDrawScreen(DX_SCREEN_BACK);
 }
